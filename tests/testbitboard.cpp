f6eb477e140cf30a52c26d2f27987ebdbb974c8d
Duncan Shepherd <shepi13>
2021-08-02 23:27:38 -0500

    Added repr and shift implementations, cleaned up code a bit.

--- tests/testbitboard.cpp
+++ tests/testbitboard.cpp
@@ -1,6 +1,8 @@
+#include "bitboard.h"
+
 #include <catch2/catch_test_macros.hpp>
 
-#include "bitboard.h"
+using shepichess::Bitboard, shepichess::Direction;
 using namespace shepichess::bitboards;
 
 TEST_CASE("bitboards::getbit", "[bitboard]") {
@@ -48,4 +50,39 @@ TEST_CASE("bitboards::popcount", "[bitboard]") {
     REQUIRE(popcount(30) == 4);
     REQUIRE(popcount(50) == 3);
     REQUIRE(popcount(0xffff'ffff'ffff'ffffULL) == 64);
+}
+
+// Test that shifting a square in the center works as expected
+TEST_CASE("bitboards::shift center", "[bitboard]") {
+    Bitboard e4 = square(27), d4 = square(28), e5 = square(35), d5 = square(36);
+    REQUIRE(shift<Direction::East>(d4) == e4);
+    REQUIRE(shift<Direction::West>(e4) == d4);
+    REQUIRE(shift<Direction::North>(e4) == e5);
+    REQUIRE(shift<Direction::South>(e5) == e4);
+
+    REQUIRE(shift<Direction::NorthEast>(d4) == e5);
+    REQUIRE(shift<Direction::SouthEast>(d5) == e4);
+    REQUIRE(shift<Direction::NorthWest>(e4) == d5);
+    REQUIRE(shift<Direction::SouthWest>(e5) == d4);
+}
+
+// Test that shifting doesn't wrap around across edges
+TEST_CASE("bitboards::shift edge", "[bitboard]") {
+    Bitboard a4 = square(31), h4 = square(24), e1 = square(3), e8=square(59);
+    // East
+    REQUIRE(shift<Direction::NorthEast>(h4) == 0);
+    REQUIRE(shift<Direction::East>(h4) == 0);
+    REQUIRE(shift<Direction::SouthEast>(h4) == 0);
+    // West
+    REQUIRE(shift<Direction::NorthWest>(a4) == 0);
+    REQUIRE(shift<Direction::West>(a4) == 0);
+    REQUIRE(shift<Direction::SouthWest>(a4) == 0);
+    // North
+    REQUIRE(shift<Direction::NorthWest>(e8) == 0);
+    REQUIRE(shift<Direction::North>(e8) == 0);
+    REQUIRE(shift<Direction::NorthEast>(e8) == 0);
+    // South
+    REQUIRE(shift<Direction::SouthWest>(e1) == 0);
+    REQUIRE(shift<Direction::South>(e1) == 0);
+    REQUIRE(shift<Direction::SouthEast>(e1) == 0);
 }
