9ff788d960d7be1d0f3de969a61ed27f15920069
Duncan Shepherd <shepi13>
2021-08-05 19:30:46 -0400

    Added tests for application code, changed global logger to be static

--- src/logging.h
+++ src/logging.h
@@ -8,7 +8,7 @@ namespace shepichess {
 
 inline void initLogging() 
 {
-  auto stderr_logger = spdlog::stderr_color_mt("shepichess_stderr_logger");
+  static auto stderr_logger = spdlog::stderr_color_mt("shepichess_stderr_logger");
   stderr_logger->set_level(spdlog::level::trace);
   spdlog::set_default_logger(stderr_logger);
   spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [thread %t] [%^%l%$] [%s:%#] %v");
