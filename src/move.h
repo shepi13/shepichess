848376c755e6c7e3cdccc336beb8a6b073dcee80
Duncan Shepherd <shepi13@gmail.com>
2021-08-14 12:47:19 -0700

    Initial class layout and data for board position

---
+++ src/move.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace shepichess
+{
+
+class Move {
+
+};
+
+} // namespace shepichess
--- src/position.h
+++ src/position.h
@@ -1,8 +1,61 @@
 #pragma once
 
+#include <vector>
+
+#include "bitboard.h"
+#include "move.h"
+#include "hash_table.h"
+
 namespace shepichess {
 
+enum class Piece {
+  WhitePawn = 0,
+  WhiteRook,
+  WhiteKnight,
+  WhiteBishop,
+  WhiteQueen,
+  WhiteKing,
+  BlackPawn = 8,
+  BlackRook,
+  BlackKnight,
+  BlackBishop,
+  BlackQueen,
+  BlackKing
+};
+
+enum class Color {
+  White,
+  Black
+};
+
+struct PositionState {
+  bool white_kingside_castle;
+  bool white_queenside_castle;
+  bool black_kingside_castle;
+  bool black_queenside_castle;
+  uint16_t move_count50;
+  uint16_t enpassant_square;
+  uint16_t material[2];
+  HashKey zobrist;
+};
+
 class Position {
+public:
+  Position();
+  ~Position();
+  Position(const Position&) = delete;
+  Position(const Position&&) = delete;
+  Position& operator=(const Position&) = delete;
+  Position& operator=(Position&&) = delete;
+
+private:
+  int move_number;
+  Color side_to_move;
+  Piece pieces[64];
+  Bitboard pieces_by_color[2];
+  Bitboard pieces_by_type[16];
+  std::vector<PositionState> states;
+  std::vector<Move> moves;
 };
 
 } // namespace shepichess
