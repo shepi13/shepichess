aedbed27c83c8b105351b6b0dbdb39bfcfe9dc04
Duncan Shepherd <shepi13@gmail.com>
2021-08-18 03:11:19 -0700

    Update hash entry to use getters, should make it easier to test and change between atomics/locks/lockless

--- src/hash_table.h
+++ src/hash_table.h
@@ -11,17 +11,25 @@ namespace shepichess {
 
 using HashKey = uint64_t;
 
-struct HashEntry {
-  int depth;
-  int eval;
-  Move best_move;
+class HashEntry {
+public:
+  HashEntry();
+  HashEntry(uint16_t depth, uint16_t eval, uint16_t best_move, HashKey key);
+  uint16_t depth() const;
+  uint16_t eval() const;
+  uint16_t bestMove() const;
+private:
+  friend class HashTable;
+  HashKey calculateChecksum();
+  HashKey data;
   HashKey key;
-  uint32_t checksum = 0;
+  HashKey checksum;
 };
 
 class HashTable {
 public:
   HashTable();
+  HashTable(size_t size);
   ~HashTable() = default;
   HashTable(const HashTable&) = delete;
   HashTable& operator=(const HashTable&) = delete;
@@ -31,9 +39,10 @@ public:
 
   void clear();
   void resize(size_t new_size_mb);
-  void stash(HashKey key, HashEntry value);
+  void stash(HashEntry value);
   HashEntry probe(HashKey key, bool& found) const;
-
+  
+  size_t size() const;
 private:
   std::vector<HashEntry> table;
   size_t hash_size;
